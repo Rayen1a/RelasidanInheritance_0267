240de076198e1e86c4c1ca59b4b3594f49958fb1
Rayen1a <raihan01426@gmail.com>
2025-05-27 12:23:08 +0700

    memasukkan int main dokter dan pasien

--- Asosisasi/asosiasi.cpp
+++ Asosisasi/asosiasi.cpp
@@ -56,3 +56,30 @@ void dokter::cetakPasien() {
     }
     cout << endl;
 }
+
+int main() {
+    dokter* varDokter1 = new dokter("dr. Budi");
+    dokter* varDokter2 = new dokter("dr.Tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahkanPasien(varPasien1);
+    varDokter1->tambahkanPasien(varPasien2);
+    varDokter2->tambahkanPasien(varPasien1);
+
+    varPasien1->tambahkanDokter(varDokter1);
+    varPasien2->tambahkanDokter(varDokter1);
+    varPasien1->tambahkanDokter(varDokter2);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+    
+    return 0;
+}
