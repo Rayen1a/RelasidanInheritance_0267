858801ee5178e6bd77a5936210e5032de4f757c1
Rayen1a <raihan01426@gmail.com>
2025-05-27 10:26:30 +0700

    first commit

---
+++ agregasi/ibu.h
@@ -0,0 +1,18 @@
+#ifnder IBU_H
+#define IBU_H
+
+class ibu {
+    public :
+        string nama;
+        vector<anak*> daftar_anak;
+
+        ibu (string pNma): nama (pNama){
+            cout << "Ibu \"" << nama << "\" ada\n";
+        }
+
+        ~ibu() {
+            cout << "Ibu \"" << nama << "\" tidak ada\n";
+        }
+        void tambahAnak(anak*);
+        void cetakAnak();
+};
